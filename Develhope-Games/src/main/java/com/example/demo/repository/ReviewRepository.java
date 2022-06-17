b0af8054eb2b84bd1c98ee34e3b422e6e8b159ae
Andrea Visentin <101669117+TheCrow28@users.noreply.github.com>
2022-06-17 12:03:15 +0200

    Delete Develhope-Games directory

--- Develhope-Games/src/main/java/com/example/demo/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
